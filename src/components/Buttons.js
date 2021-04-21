6c045123047d32f5ec0a2dad3fb054d9daa11ddb
Zach Heckert <zach@zachheckert.com>
2021-04-21 16:39:05 -0600

    ok so now each button creates an array and shows previous numbers in the array. The only issue is that each button is its own entity with its own state object. Maybe context would fix this. Still, progress

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -7,38 +7,12 @@ export const Buttons = (props) => {
 
     let arr = []
 
-    const funcPush = (num) => {
-        console.log("This is num", num)
-        setNumArray((props.button))
-        arr.push(num)
-        console.log("Here is my array", arr)
-    }
+    console.log(numArray)
 
     return(
         <div className="gridChild">
-            {/* <Button variant="contained" onClick={() => (console.log(`${props.button} was clicked`))}>{props.button}</Button> */}
-            <Button variant="contained" onClick={() => funcPush(props.button)}
-                >{props.button}</Button>
+            <Button variant="contained" onClick={() => setNumArray((numArray) => [...numArray, props.button])}
+            >{props.button}</Button>
         </div>
     )
 }
-
-//old and janky below
-// export const Buttons = (props) => {
-
-//     let arr = []
-
-//     const funcPush = (num) => {
-//         console.log(num)
-//         arr.push(num)
-//         console.log(arr)
-//     }
-
-//     return(
-//         <div className="gridChild">
-//             {/* <Button variant="contained" onClick={() => (console.log(`${props.button} was clicked`))}>{props.button}</Button> */}
-//             <Button variant="contained" onClick={() => funcPush(props.button)}
-//                 >{props.button}</Button>
-//         </div>
-//     )
-// }
